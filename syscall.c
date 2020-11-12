4d94e8727af502d425d19acbdef3a0eb7629bdf8
aloktiwa <alok9696979@gmail.com>
2020-11-12 10:58:15 +0530

    syscall file

---
+++ syscall.c
@@ -0,0 +1,14 @@
+#include <sys/syscall.h>
+//#include <stdio.h>
+
+long long base;
+int main()
+{
+    char buff[30];
+    syscall(SYS_write,1,"Hello workd\n",14);
+//  read(0,&buff,20);
+//  base = puts("===");
+//  puts(buff);
+    //printf("Hello world\n");
+    //write(1,"Hello world\n",14);
+}
